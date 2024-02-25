8a7d3adc9043871cd5274bb302150720f19f1eac
Marcio Barroso <sigsegov@gmail.com>
2024-02-25 17:43:13 -0300

    create components

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from "./components/Header"
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import TaskDetails from "./components/TaskDetails";
+
+import './App.css'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+    }
+  ])
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed }
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [...tasks, {
+      title: taskTitle,
+      id: uuidv4(),
+      completed: false
+    }]
+
+    setTasks(newTasks)
+  }
+
+  const handleTaskDelition = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId)
+
+    setTasks(newTasks)
+  }
+
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+        <Route 
+          path='/'
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks 
+                tasks={tasks} 
+                handleTaskClick={handleTaskClick}
+                handleTaskDelition={handleTaskDelition}
+              />
+            </>
+          )}
+        />
+        <Route path="/:taskTitle" exact component={TaskDetails}/>
+      </div>
+    </ Router>
+  )
+}
+
+export default App;
